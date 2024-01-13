9d189b80d0e821a7fd9c53c5c8a7f420d19ea932
Robert Ellen <rellen@ieee.org>
2024-01-13 17:24:19 +1000

    mix project

---
+++ mix.exs
@@ -0,0 +1,38 @@
+defmodule Expresso.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :expresso,
+      version: "0.1.0",
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # docs
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+
+      # check
+      {:ex_check, "~> 0.15", only: :dev},
+
+      # static analysis
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+      {:doctor, ">= 0.0.0", only: :dev, runtime: false},
+      {:mix_audit, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:sobelow, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+end
